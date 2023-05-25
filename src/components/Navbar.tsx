b9f02f549742a60c5b7d8a3ec1778dff6251b59c
R0B0xC0DER <r0b0xc0der@R0B0xC0DERs-iMac.local>
2023-05-25 22:28:47 +0300

    edit navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,14 +6,14 @@ export default function Navbar() {
   const $currentSection = useStore(currentSection);
 
   const disableNav =
-    "cursor-pointer hover:text-[#e2620c] py-[0.3rem] px-3  rounded-lg ";
+    "cursor-pointer hover:text-[#e2620c] py-[0.3rem] px-[0.7rem] sm:px-3  rounded-lg ";
   const enableNav =
-    "cursor-pointer  py-[0.3rem] px-3  rounded-lg bg-[#ff3e17]/10 text-[#ff3e17]";
+    "cursor-pointer  py-[0.3rem] px-[0.7rem] sm:px-3  rounded-lg bg-[#ff3e17]/10 text-[#ff3e17]";
 
   return (
-    <header className=" w-full h-52 flex items-center justify-center mb-10 ">
-      <nav className="">
-        <ul className=" font-bold  rounded-lg flex items-center justify-center gap-1 sm:gap-3  bg-slate-300/50">
+    <header className="">
+      <nav className="w-full h-52 flex items-center justify-center mb-10">
+        <ul className=" font-bold rounded-lg flex flex-wrap  sm:gap-3  bg-slate-300/50">
           <li 
 		  onClick={() => currentSection.set("about")}
 		  className={$currentSection === "about" ? enableNav : disableNav}>
